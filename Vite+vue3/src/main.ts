c5e3767e62d6dff87d167d39820026e64d23c272
pengcheng433 <773784840@qq.com>
2022-01-30 16:25:57 +0800

    createRouter成功

--- Vite+vue3/src/main.ts
+++ Vite+vue3/src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createWebHashHistory, createRouter } from "vue-router"
 
-createApp(App).mount('#app')
+import HellowWorld from "./components/HelloWorld.vue"
+
+const history = createWebHashHistory()
+const Router = createRouter({ history:history, routes: [{ path: "/home", component: HellowWorld }] })
+const app = createApp(App);
+app.use(Router);
+app.mount('#app')
