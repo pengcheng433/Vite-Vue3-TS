3aa138eb4ca5698e0b5db199e0d811d6a246b0b3
DESKTOP-610Q01O\PsyDuck <773784840@qq.com>
2022-02-09 16:52:19 +0800

    调节topnav和aside的样式

--- Vite+vue3/src/views/Doc.vue
+++ Vite+vue3/src/views/Doc.vue
@@ -31,5 +31,23 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  padding-top: 70px;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
 </style>
