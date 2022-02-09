f587ec8410b6f4d31f1b6e352662f3a4548dbf41
DESKTOP-610Q01O\PsyDuck <773784840@qq.com>
2022-02-09 16:27:57 +0800

    初始化l侧边栏

--- Vite+vue3/src/views/Doc.vue
+++ Vite+vue3/src/views/Doc.vue
@@ -2,7 +2,25 @@
   <div>
    <Topnav />
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">tabs 组件</router-link>
+          </li>
+         
+
+        </ol>
+      </aside>
       <main>主内容</main>
     </div>
   </div>
