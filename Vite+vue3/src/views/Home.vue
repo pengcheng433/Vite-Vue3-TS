c97bd01db69b8cac6f1692252b832bf7012daf47
DESKTOP-610Q01O\PsyDuck <773784840@qq.com>
2022-02-09 15:48:01 +0800

    Home 页面样式

--- Vite+vue3/src/views/Home.vue
+++ Vite+vue3/src/views/Home.vue
@@ -20,20 +20,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.topnav {
-  background: pink;
+.banner {
+  padding: 100px 0;
   display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
     }
   }
 }
