921057d7ae14456d9df739d734810feccea4049c
DESKTOP-610Q01O\PsyDuck <773784840@qq.com>
2022-02-10 15:07:40 +0800

    实现aside切换

--- Vite+vue3/src/App.vue
+++ Vite+vue3/src/App.vue
@@ -1,13 +1,15 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-// import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <router-view />
 </template>
-
+<script  lang="ts">
+import { provide, ref } from "vue";
+export default {
+  name: "App",
+  setup() {
+    const asideVisible = ref(false);
+    provide("getmenuVisible",asideVisible)
+  },
+};
+</script>
 <style>
-
 </style>
--- Vite+vue3/src/views/Home.vue
+++ Vite+vue3/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Topnav />
+    <Topnav  />
     <div class="banner">
       <h1>轱辘UI</h1>
       <h2>一个厉害的 UI 框架</h2>
