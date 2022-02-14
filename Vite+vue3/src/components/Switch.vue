b2a9e8cf8f8d3c25fac316a08a5ceaba1a530d07
DESKTOP-610Q01O\PsyDuck <773784840@qq.com>
2022-02-14 15:31:57 +0800

    switch 切换

--- Vite+vue3/src/components/Switch.vue
+++ Vite+vue3/src/components/Switch.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <button><span></span></button>
+    <button :class="{ checked }" @click="toggle"><span></span></button>
   </div>
 </template>
 
 <script lang="ts">
-export default {};
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return { checked, toggle };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -15,7 +24,7 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: blue;
+  background: gray;
   border-radius: $h/2;
   position: relative;
 }
@@ -28,8 +37,10 @@ span {
   background: white;
   border-radius: $h2 / 2;
 }
-button:hover > span {
+button.checked {
+  background: blue;
+}
+button.checked > span {
   left: calc(100% - #{$h2} - 2px);
-  
 }
 </style>
