e2ac874928a11f8009745eb8ffb30499d96aa9c2
DESKTOP-610Q01O\PsyDuck <773784840@qq.com>
2022-02-15 16:51:50 +0800

    使用inheritAttrs、...rest、

--- Vite+vue3/src/components/ButtonDemo.vue
+++ Vite+vue3/src/components/ButtonDemo.vue
@@ -1,9 +1,18 @@
 <template>
-  <button><slot /></button>
+  <div :class="size">
+    <button v-bind="rest"><slot /></button>
+  </div>
 </template>
 
 <script lang="ts">
-export default {};
+export default {
+  inheritAttrs: false,
+  setup(props, context) {
+    console.log(context);
+    const { size, ...rest } = context.attrs;
+    return { size, rest };
+  },
+};
 </script>
 
 <style>
--- Vite+vue3/src/lib/Button.vue
+++ Vite+vue3/src/lib/Button.vue
@@ -1,11 +1,23 @@
 <template>
-  <Button>按钮</Button>
+  <div>Butoon 示例</div>
+  <h1>示例1</h1>
+  <div>
+    <Button size="small" @mouseover="onclick" @focus="onclick" @click="onclick"
+      >按钮</Button
+    >
+  </div>
 </template>
 
 <script lang="ts">
 import Button from "../components/ButtonDemo.vue";
 export default {
   components: { Button },
+  setup() {
+    const onclick = () => {
+      console.log("你好");
+    };
+    return { onclick };
+  },
 };
 </script>
 
