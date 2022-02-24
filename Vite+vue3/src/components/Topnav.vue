a71055c6cd8d2625548cf68204a8b556f146dbcc
DESKTOP-610Q01O\PsyDuck <773784840@qq.com>
2022-02-24 15:13:51 +0800

    首页样式优化布局和icon

--- Vite+vue3/src/components/Topnav.vue
+++ Vite+vue3/src/components/Topnav.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <router-link to="/" class="logo" >
+      <svg class="icon">
+        <use xlink:href="#icon-psyduck"></use>
+      </svg>
+    </router-link >
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
     </ul>
-    <span class="toggleAside" @click="toggleMenu"></span>
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+      <use xlink:href="#icon-menu"></use>
+    </svg>
   </div>
 </template>
 <script lang="ts">
 import { inject, Ref } from "vue";
 export default {
+  props: {
+    toggleMenuButtonVisible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
   setup() {
     const asideVisible = inject<Ref<boolean>>("getmenuVisible");
     const toggleMenu = () => {
@@ -21,20 +35,28 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$color: #007974;
+
 .topnav {
-  background: pink;
+  // background: pink;
+  color: $color;
+
   display: flex;
   padding: 16px;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  z-index: 10;
+  z-index: 20;
   justify-content: center;
   align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    > svg {
+      width: 32px;
+      height: 32px;
+    }
   }
   > .menu {
     display: flex;
@@ -45,23 +67,23 @@ export default {
     }
   }
   > .toggleAside {
-    width: 24px;
-    height: 24px;
-    background: red;
+    width: 32px;
+    height: 32px;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
     display: none;
+    background: fade-out(black, 0.9);
   }
   @media (max-width: 500px) {
-    > .menu {
+      >.menu {
       display: none;
     }
-    > .logo {
+    >.logo {
       margin: 0 auto;
     }
-    > .toggleAside {
+    >.toggleAside {
       display: inline-block;
     }
   }
